069f855338e8030f33616b9ccd2bf24ff7cbe87c
lady-coder-16 <ayortiz16@gmail.com>
2023-02-28 23:45:29 -0300

    Se agrega checkbox en la seccion de Habilidades y se crea funcion para que cuando se haga click en ellos, siempre queden marcados

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 100);
 });
+
+
+/* Cada vez que se hace click en uno de los checkbox de Habilidad,
+se vuelven a tildar todos.*/
+
+const bloque_skills = document.getElementById("listado-habilidades");
+console.log(bloque_skills);
+bloque_skills.addEventListener("click",tildarTodosLosCheckbox);
+
+function tildarTodosLosCheckbox(){
+    for(var i=1; i<12; i++){
+        palabraUsandoContenidoVariable="skill_"+i;
+
+        var skill= document.getElementById(palabraUsandoContenidoVariable);
+        console.log(skill);
+
+        skill.checked=true;
+    }
+}
